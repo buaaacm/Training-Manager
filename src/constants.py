29803453e28b1c132d7d8a2877adf1a87855aa31
Xuanang Zhao <Sd.Invol@gmail.com>
2018-05-14 22:12:42 +0800

    Commit

--- src/constants.py
+++ src/constants.py
@@ -19,18 +19,121 @@ id_to_team_name_2017 = {
   'team773': u'极值点',
 }
 
-id_to_team_name_2015 = {
-  'team103': u'TheWaySoFar',
-  'team104': u'悬顶之剑',
-  'team105': u'undetermined',
-  'team106': u'体锻线',
-  'team107': u'甜甜圈',
-  'team108': u'新蜂人',
-  'team109': u'三个火枪手',
-  'team541': u'沛沛沛',
-  'team542': u'普罗米修斯',
-  'team543': u'忆当年,穿着细事且莫等闲看',
-  'team544': u'孤独的根号三',
-  'team545': u'三剑客',
-  'team546': u'还没定下来',
+id_to_team_name_ccpc_2017 = {
+  'team0506': u'黑人问号.jpg',
+  'team0507': u'水能载舟',
+  'team0508': u'CE使我快乐',
+  'team0509': u'复苏',
+  'team0510': u'我们吓成一团了',
+  'team0511': u'TAT',
+  'team0512': u'头文件',
+  'team0513': u'人生已经如此的艰难',
+  'team0514': u'我需要治疗',
+  'team0515': u'终结者',
+  'team0516': u'不存在的',
+  'team0517': u'tvcr',
+  'team0518': u'极值点',
+  'team0519': u'瞎搞',
 }
+
+id_to_team_name_shenyang_2017 = {
+  'team0073': u'黑人问号.jpg',
+  'team0074': u'水能载舟',
+  'team0075': u'CE使我快乐',
+  'team0076': u'复苏',
+  'team0077': u'我们吓成一团了',
+  'team0078': u'TAT',
+  'team0079': u'头文件',
+  'team0080': u'人生已经如此的艰难',
+  'team0081': u'我需要治疗',
+  'team0082': u'终结者',
+  'team0083': u'不存在的',
+  'team0084': u'tvcr',
+  'team0085': u'极值点',
+  'team0086': u'瞎搞',
+}
+
+id_to_team_name_qingdao = {
+  'team0305': u'黑人问号.jpg',
+  'team0306': u'水能载舟',
+  'team0307': u'CE使我快乐',
+  'team0308': u'复苏',
+  'team0309': u'我们吓成一团了',
+  'team0310': u'TAT',
+  'team0311': u'头文件',
+  'team0312': u'人生已经如此的艰难',
+  'team0313': u'我需要治疗',
+  'team0314': u'终结者',
+  'team0315': u'不存在的',
+  'team0317': u'tvcr',
+  'team0318': u'极值点',
+}
+
+
+id_to_team_name_xian = {
+  'team38': u'黑人问号.jpg',
+  'team39': u'水能载舟',
+  'team40': u'CE使我快乐',
+  'team41': u'复苏',
+  'team42': u'我们吓成一团了',
+  'team43': u'TAT',
+  'team44': u'头文件',
+  'team45': u'人生已经如此的艰难',
+  'team46': u'我需要治疗',
+  'team47': u'终结者',
+  'team48': u'不存在的',
+  'team50': u'tvcr',
+  'team51': u'极值点',
+}
+
+
+id_to_team_name_urumqi = {
+  'team270': u'黑人问号.jpg',
+  'team271': u'水能载舟',
+  'team272': u'CE使我快乐',
+  'team273': u'复苏',
+  'team274': u'我们吓成一团了',
+  'team275': u'TAT',
+  'team276': u'头文件',
+  'team277': u'人生已经如此的艰难',
+  'team278': u'我需要治疗',
+  'team279': u'终结者',
+  'team280': u'不存在的',
+  'team281': u'tvcr',
+  'team282': u'极值点',
+  'team283': u'瞎搞',
+}
+
+id_to_team_name_nanning = {
+  'team270': u'黑人问号.jpg',
+  'team271': u'水能载舟',
+  'team272': u'CE使我快乐',
+  'team273': u'复苏',
+  'team274': u'我们吓成一团了',
+  'team275': u'TAT',
+  'team276': u'头文件',
+  'team277': u'人生已经如此的艰难',
+  'team278': u'我需要治疗',
+  'team279': u'终结者',
+  'team280': u'不存在的',
+  'team281': u'tvcr',
+  'team282': u'极值点',
+  'team283': u'瞎搞',
+}
+
+id_to_team_name_beijing = {
+  'team170465': u'黑人问号.jpg',
+  'team170466': u'水能载舟',
+  'team170468': u'CE使我快乐',
+  'team170469': u'复苏',
+  'team170470': u'我们吓成一团了',
+  'team170471': u'TAT',
+  'team170472': u'头文件',
+  'team170473': u'人生已经如此的艰难',
+  'team170474': u'我需要治疗',
+  'team170475': u'终结者',
+  'team170476': u'不存在的',
+  'team170477': u'tvcr',
+  'team170478': u'极值点',
+}
+
